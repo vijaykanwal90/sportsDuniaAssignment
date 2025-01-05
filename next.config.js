b0dbcf397009609f8c5f294b3f77bdfa1eb4e7aa
vijaykanwal <vijaykanwal90@gmail.com>
2025-01-06 01:29:52 +0530

    decreased version of nextjs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['bilder3.n-tv.de','ml.globenewswire.com' ,'media.zenfs.com'], // Add the hostname of your image source
-  },
-
+    domains: [
+      'bilder3.n-tv.de',
+      'ml.globenewswire.com',
+      'media.zenfs.com'
+    ]
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
